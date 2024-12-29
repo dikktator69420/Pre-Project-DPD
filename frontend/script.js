6a4ae3e0ef99f3d1d5280c52ae3c8a3b16468228
dikktator69420 <168074906+ROT210575@users.noreply.github.com>
2024-12-29 19:50:53 +0100

    Land existiert

--- frontend/script.js
+++ frontend/script.js
@@ -6,8 +6,10 @@ async function Submit() {
   var Türnummer = document.getElementById("Türnummer").value;
   var PLZ = document.getElementById("PLZ").value;
   var Ort = document.getElementById("Ort").value;
+  var Land = document.getElementById("Land").value;
+
   var result = await fetch(
-    `${URL}/${Straße}/${Hausnummer}/${Türnummer}/${PLZ}/${Ort}`
+    `${URL}/${Straße}/${Hausnummer}/${Türnummer}/${PLZ}/${Ort}/${Land}`
   ).then((response) => response.json());
   document.getElementById("result").innerHTML = result.value;
 }
