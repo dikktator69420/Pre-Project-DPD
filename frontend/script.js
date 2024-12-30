f2518228f51cab67a678d66353d8c97fbafe0e72
dikktator69420 <168074906+ROT210575@users.noreply.github.com>
2024-12-30 20:02:23 +0100

    Update script.js
    Finished frontend for testing

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
-async function Submit() {
-  const URL = "placeholder";
+const URL = "placeholder";
 
+async function Submit() {
   var Straße = document.getElementById("Straße").value;
   var Hausnummer = document.getElementById("Hausnummer").value;
   var Türnummer = document.getElementById("Türnummer").value;
@@ -8,8 +8,19 @@ async function Submit() {
   var Ort = document.getElementById("Ort").value;
   var Land = document.getElementById("Land").value;
 
-  var result = await fetch(
-    `${URL}/${Straße}/${Hausnummer}/${Türnummer}/${PLZ}/${Ort}/${Land}`
-  ).then((response) => response.json());
-  document.getElementById("result").innerHTML = result.value;
+  const response = await fetch(`${URL}/process.php`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      strings: [Straße, Hausnummer, Türnummer, PLZ, Ort, Land],
+    }),
+  });
+
+  const data = await response.json();
+  document.getElementById("result").textContent =
+    data.status === "success" ? data.result : data.message;
+
+  //This is still theoretical, process.php doesnt exist yet, however, this is a functionally complete frontend
 }
