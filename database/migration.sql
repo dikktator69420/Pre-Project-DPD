1eaa63f1c2b52e71802d1736a942197885a88757
Finian Pichler <pic210572@spengergasse.at>
2025-01-09 10:02:13 +0100

    Fixed Migration file

--- database/migration.sql
+++ database/migration.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE if not exists address_validator;
+use address_validator;
+
 DROP TABLE IF EXISTS addresses;
 
 CREATE TABLE addresses (
@@ -5,21 +8,26 @@ CREATE TABLE addresses (
     straße VARCHAR(255),
     haus_nummer VARCHAR(50),
     plz VARCHAR(20),
-    stadt VARCHAR(100),
-    land CHAR(2)
+    stadt VARCHAR(100)
 );
+
+
+DROP TABLE IF EXISTS ortschaften;
 
 CREATE TABLE ortschaften (
     id VARCHAR(5) PRIMARY KEY,
-    ort_name VARCHAR(255);
+    ort_name VARCHAR(255)
 );
+
+
+DROP TABLE IF EXISTS strassen;
 
 CREATE TABLE strassen (
     id VARCHAR(6) PRIMARY KEY,
-    strasse_name VARCHAR(255);
+    strasse_name VARCHAR(255)
 );
 
-LOAD DATA INFILE 'ORTSCHAFT.csv' --csv files are too large for github, sent in project files channel on discord
+LOAD DATA INFILE 'ORTSCHAFT.csv' 
 INTO TABLE ortschaften
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
@@ -27,13 +35,13 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (@GKZ, @OKZ, @ORTSNAME)  
 SET 
-    id = @OKZ
+    id = @OKZ,
     ort_name = @ORTSNAME;
 
 
 
 
-LOAD DATA INFILE 'STRASSE.csv' --csv files are too large for github, sent in project files channel on discord
+LOAD DATA INFILE 'STRASSE.csv' 
 INTO TABLE strassen
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
@@ -41,13 +49,13 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (@SKZ, @STRASSENNAME, @STRASSENNAMENZUSATZ, @SZUSADRBEST, @GKZ, @ZUSTELLORT, @ZUSTELLORT_ID)  
 SET 
-    id = @SKZ
+    id = @SKZ,
     strasse_name = @STRASSENNAME;  
 
 
 
 
-LOAD DATA INFILE 'ADRESSE.csv' --csv files are too large for github, sent in project files channel on discord
+LOAD DATA INFILE 'ADRESSE.csv' 
 INTO TABLE addresses
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
@@ -55,13 +63,13 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (@ADRCD, @GKZ, @OKZ, @PLZ, @SKZ, @ZAEHLSPRENGEL, @HAUSNRTEXT, @HAUSNRZAHL1, @HAUSNRBUCHSTABE1, @HAUSNRVERBINDUNG1, @HAUSNRZAHL2, @HAUSNRBUCHSTABE2, @HAUSNRBEREICH, @HNR_ADR_ZUSAMMEN, @GNRADRESSE, @HOFNAME, @RW, @HW, @EPSG, @QUELLADRESSE, @BESTIMMUNGSART)  
 SET 
-    id = @ADRCD
+    id = @ADRCD,
     straße = (
         SELECT strasse_name
         FROM strassen
         WHERE id = @sKZ
-    ), 
-    haus_nummer = concat(@HAUSNRTEXT @HAUSNRZAHL1, @HAUSNRBUCHSTABE1, @HAUSNRVERBINDUNG1, @HAUSNRZAHL2, @HAUSNRBUCHSTABE2, @HAUSNRBEREICH),
+    ),
+    haus_nummer = concat(@HAUSNRTEXT, @HAUSNRZAHL1, @HAUSNRBUCHSTABE1, @HAUSNRVERBINDUNG1, @HAUSNRZAHL2, @HAUSNRBUCHSTABE2, @HAUSNRBEREICH),
     plz = @PLZ,
     stadt = (
         SELECT ort_name
